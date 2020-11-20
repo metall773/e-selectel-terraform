7def237b10de3a8e2376df7a919f38dcec70b622
Evgeniy S <lee040404@gmail.com>
2020-11-20 13:46:33 +0100

    commit for pipeline debug stating

--- backend.tf
+++ backend.tf
@@ -1,13 +1,18 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate"
-    key    = "dev/terraform.tfstate"
-    region = "ru-1a"
-    endpoint = "https://s3.selcdn.ru"
-    access_key = "137805"
-    secret_key = "+FPQm$/{1I"
-    skip_region_validation = true
+    bucket                      = "tfstate"
+    key                         = "dev/dev_terraform.tfstate"
+    region                      = "ru-1a"
+    endpoint                    = "https://s3.selcdn.ru"
+    access_key                  = "137805"
+    secret_key                  = "+FPQm$/{1I"
+    skip_region_validation      = true
     skip_credentials_validation = true
-    force_path_style = true
+    force_path_style            = true
   }
+}
+
+# Initialize Selectel provider with token.
+provider "selectel" {
+  token = var.sel_token
 }
