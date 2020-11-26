670bd84bfab3dd73560ad6c5fe74f25f9272bdfc
Evgeniy S <lee040404@gmail.com>
2020-11-26 14:37:22 +0100

    ru-2

--- vars.tf
+++ vars.tf
@@ -25,7 +25,7 @@ variable "os_auth_url" {
 }
 
 variable "os_region" {
-  default = "ru-7"
+  default = "ru-2"
 }
 
 variable "server_name" {
@@ -33,7 +33,7 @@ variable "server_name" {
 }
 
 variable "server_zone" {
-  default = "ru-7a"
+  default = "ru-2a"
 }
 
 variable "server_vcpus" {
