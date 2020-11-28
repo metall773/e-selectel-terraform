42a6176fc65a9dac8f950f9894b1508ea8d164a7
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-11-28 20:45:50 +0100

    net 8

--- network.tf
+++ network.tf
@@ -1,6 +1,14 @@
 module "network" {
   source = "git@gitlab.com:lee040404/e-selectel-terraform-modules.git//modules/vpc/network"
 
+  # OpenStack auth.
+  os_project_name  = local.os_project_name
+  os_user_name     = var.user_name
+  os_user_password = var.user_password
+  os_domain_name   = var.sel_account
+  os_auth_url      = var.os_auth_url
+  os_region        = var.os_region
+
   router_external_net_name = "${var.environment}-external_network"
   router_name              = "${var.environment}-router"
   network_name             = "${var.environment}-network"
