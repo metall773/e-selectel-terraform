1f2ca0c5318b2141c68652a3eb340f0097432085
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-11-28 20:36:58 +0100

    net 5

--- network.tf
+++ network.tf
@@ -1,8 +1,8 @@
 module "network" {
   source = "git@gitlab.com:lee040404/e-selectel-terraform-modules.git//modules/vpc/network"
 
-  router_external_net_name = "external_network"
-  router_name              = "router"
-  network_name             = "network"
+  router_external_net_name = "${var.environment}-external_network"
+  router_name              = "${var.environment}-router"
+  network_name             = "${var.environment}-network"
   subnet_cidr              = "192.168.70.0/24"
 }
