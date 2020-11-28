fb48360286b91053127249538349f5ab3613fcbc
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-11-28 23:24:02 +0100

    net 31

--- dns.tf
+++ dns.tf
@@ -1,4 +1,4 @@
 #https://registry.terraform.io/providers/selectel/selectel/latest/docs/resources/domains_record_v1
 resource "selectel_domains_domain_v1" "domain" {
-  name = "local.dns_domain
+  name = local.dns_domain
 }
