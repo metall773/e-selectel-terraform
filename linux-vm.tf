a5658b8ca3f7ea72525c9e0887c1436143d2b5b9
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-11-29 01:18:42 +0100

    add bastion

--- linux-vm.tf
+++ linux-vm.tf
@@ -62,3 +62,33 @@ module "lb01" {
   vm_firewall_tcp_ports = "22 80 443"
   enable_floatingip     = true
 }
+
+module "bastion01" {
+  source = "git@gitlab.com:lee040404/e-selectel-terraform-modules.git//modules/vpc/centos8"
+
+  # OpenStack auth.
+  os_project_name  = local.os_project_name
+  os_user_name     = var.user_name
+  os_user_password = var.user_password
+  os_domain_name   = var.sel_account
+  os_auth_url      = var.os_auth_url
+  os_region        = var.os_region
+
+  # OpenStack Instance parameters.
+  server_name         = "bastion01"
+  server_zone         = var.server_zone
+  server_vcpus        = var.server_vcpus
+  server_ram_mb       = var.server_ram_mb
+  server_root_disk_gb = var.server_root_disk_gb
+  server_image_name   = "CentOS 8 64-bit"
+  server_ssh_key      = file("~/.ssh/id_rsa.pub")
+  server_ssh_key_user = module.project_with_user.user_id
+  network_id          = module.network.network_id
+  subnet_id           = module.network.subnet_id
+  vm_dns_domain_id    = selectel_domains_domain_v1.domain.id
+  vm_dns_domain_name  = local.dns_domain
+
+  vm_packages_4_install = "mc nmon htop"
+  vm_firewall_tcp_ports = "22"
+  enable_floatingip     = true
+}
