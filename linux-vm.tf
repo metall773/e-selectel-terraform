984050291708b900a9fe0c72213a5b1c43017487
Evgeniy S <lee040404@gmail.com>
2020-12-01 18:25:31 +0100

    centos8 bitrix test

--- linux-vm.tf
+++ linux-vm.tf
@@ -21,6 +21,29 @@ module "bitrix01" {
   vm_install_bitrix     = "yes"
 }
 
+module "bitrix02" {
+  source = "git@gitlab.com:lee040404/e-selectel-terraform-modules.git//modules/vpc/centos"
+
+  # OpenStack Instance parameters.
+  server_name         = "bitrix01"
+  server_zone         = var.server_zone
+  server_vcpus        = var.server_vcpus
+  server_ram_mb       = var.server_ram_mb
+  server_root_disk_gb = var.server_root_disk_gb
+  server_ssh_key      = file("~/.ssh/id_rsa.pub")
+  server_ssh_key_user = module.project_with_user.user_id
+  network_id          = module.network.network_id
+  subnet_id           = module.network.subnet_id
+  vm_dns_domain_id    = selectel_domains_domain_v1.domain.id
+  vm_dns_domain_name  = local.dns_domain
+
+  vm_packages_4_install = "mc nmon htop"
+  vm_firewall_tcp_ports = "22 80 443"
+  vm_firewall_sshd_net  = var.localnet
+  vm_install_bitrix     = "yes"
+}
+
+
 module "lb01" {
   source = "git@gitlab.com:lee040404/e-selectel-terraform-modules.git//modules/vpc/centos"
 
