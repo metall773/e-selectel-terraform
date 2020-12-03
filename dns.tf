cb8a1a39abc1cd9cafad745afdfa0d49d65c945c
Evgeniy S <lee040404@gmail.com>
2020-12-03 18:52:54 +0100

    dns fix6

--- dns.tf
+++ dns.tf
@@ -3,8 +3,8 @@ resource "selectel_domains_domain_v1" "domain" {
   name = local.dns_domain
 }
 
-resource "selectel_domains_record_v1" "cname_record_1" {
-  for_each module.bitrix
+resource "selectel_domains_record_v1" "bitrix_cname_records" {
+  for_each  = module.bitrix
   domain_id = selectel_domains_domain_v1.domain.id
   name      = "${each.value.server_name}.${local.dns_domain}"
   type      = "CNAME"
