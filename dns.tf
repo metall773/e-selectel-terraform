630455766fd12baa1e9c19a8be629542b7e0e683
Evgeniy S <lee040404@gmail.com>
2020-12-03 18:00:34 +0100

    add cname to lb

--- dns.tf
+++ dns.tf
@@ -2,3 +2,11 @@
 resource "selectel_domains_domain_v1" "domain" {
   name = local.dns_domain
 }
+
+resource "selectel_domains_record_v1" "cname_record_1" {
+  domain_id = selectel_domains_domain_v1.domain.id
+  name = "bitrix01.${local.dns_domain}"
+  type = "CNAME"
+  content = module.lb01.server_FQDN
+  ttl = 60
+}
