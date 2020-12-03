97ee15dcc81f0b100753e0a4e4df8a0ea4820775
Evgeniy S <lee040404@gmail.com>
2020-12-03 18:51:05 +0100

    dns fix5

--- dns.tf
+++ dns.tf
@@ -3,10 +3,11 @@ resource "selectel_domains_domain_v1" "domain" {
   name = local.dns_domain
 }
 
-#resource "selectel_domains_record_v1" "cname_record_1" {
-#  domain_id = selectel_domains_domain_v1.domain.id
-#  name      = "${module.bitrix[0].server_name}.${local.dns_domain}"
-#  type      = "CNAME"
-#  content   = module.lb01.server_FQDN
-#  ttl       = 60
-#}
+resource "selectel_domains_record_v1" "cname_record_1" {
+  for_each module.bitrix
+  domain_id = selectel_domains_domain_v1.domain.id
+  name      = "${each.value.server_name}.${local.dns_domain}"
+  type      = "CNAME"
+  content   = module.lb01.server_FQDN
+  ttl       = 60
+}
