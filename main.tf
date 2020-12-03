48dff1fc7446f7f052f086612c9a888844ba2373
Evgeniy S <lee040404@gmail.com>
2020-12-03 18:25:13 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ locals {
   bitrix_vm = {
     "catalog-merlion" = {
       server_vcpus        = 4
-      server_ram_mb       = 8
+      server_ram_mb       = 8000
       server_root_disk_gb = 80
     },
     "multibonus" = {
       server_vcpus        = 4
-      server_ram_mb       = 8
+      server_ram_mb       = 8000
       server_root_disk_gb = 80
     },
     "urozhai" = {
       server_vcpus        = 4
-      server_ram_mb       = 8
+      server_ram_mb       = 8000
       server_root_disk_gb = 80
     }
   }
