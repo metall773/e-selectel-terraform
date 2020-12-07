66c2c35a58e8699166c33de56f240f5cdae11dbd
Evgeniy S <lee040404@gmail.com>
2020-12-07 12:05:34 +0100

    catalog-merlion bitrix

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ locals {
   user_name       = "${var.environment}-tf-user"
 
   bitrix_vm = {
-    "multibonus" = {
+    "catalog-merlion" = {
       server_vcpus        = 4
       server_ram_mb       = 8000
       server_root_disk_gb = 80
