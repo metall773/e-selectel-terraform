6fe686339fbf38cf5ab991972159301861dad20b
Evgeniy S <lee040404@gmail.com>
2020-12-07 19:54:50 +0100

    add  depends_on

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source = "git@gitlab.com:lee040404/e-selectel-terraform-modules.git//modules/vpc/network"
-  depends_on = [module.project_with_user
+  depends_on = [module.project_with_user]
   router_external_net_name = "external-network"
   router_name              = "${var.environment}-router"
   network_name             = "${var.environment}-network"
