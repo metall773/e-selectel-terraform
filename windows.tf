3f0e6090b770e2e32e38e0f44b7ab8c14a7a480a
Evgeniy Sevostyanov <evgeniy.sevostyanov@tipico.com>
2020-12-14 22:34:57 +0000

    terraform fmt

--- windows.tf
+++ windows.tf
@@ -1,20 +1,20 @@
 module "win01" {
-  source = "git@gitlab.com:lee040404/e-selectel-terraform-modules.git//modules/vpc/windows"
+  source     = "git@gitlab.com:lee040404/e-selectel-terraform-modules.git//modules/vpc/windows"
   depends_on = [module.project_with_user]
   # OpenStack Instance parameters.
-  server_name         = "win01"
-  server_zone         = var.server_zone
-  server_vcpus        = 1
-  server_ram_mb       = 4096
-  server_root_disk_gb = 32
-  network_id          = module.network.network_id
-  subnet_id           = module.network.subnet_id
-  vm_dns_domain_id    = selectel_domains_domain_v1.domain.id
-  vm_dns_domain_name  = local.dns_domain
-  server_image_name   = "Windows Server 2016 Standard"
-  admin_pass          = "Ghdbtn123!"
+  server_name               = "win01"
+  server_zone               = var.server_zone
+  server_vcpus              = 1
+  server_ram_mb             = 4096
+  server_root_disk_gb       = 32
+  network_id                = module.network.network_id
+  subnet_id                 = module.network.subnet_id
+  vm_dns_domain_id          = selectel_domains_domain_v1.domain.id
+  vm_dns_domain_name        = local.dns_domain
+  server_image_name         = "Windows Server 2016 Standard"
+  admin_pass                = "Ghdbtn123!"
   server_second_volume_type = "fast.${var.server_zone}"
-  server_second_disk_gb  = 32
+  server_second_disk_gb     = 32
 
-  enable_floatingip   = true
+  enable_floatingip = true
 }
