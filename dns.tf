e3a669fcb488268cc035355c79968b77ee65b887
Evgeniy S <lee040404@gmail.com>
2020-12-17 19:31:08 +0100

    add CNAME for cloudcontent and images

--- dns.tf
+++ dns.tf
@@ -11,3 +11,21 @@ resource "selectel_domains_record_v1" "bitrix_cname_records" {
   content   = module.lb01.server_FQDN
   ttl       = 60
 }
+
+resource "selectel_domains_record_v1" "images_cname_records" {
+  for_each  = module.bitrix
+  domain_id = selectel_domains_domain_v1.domain.id
+  name      = "images.${local.dns_domain}"
+  type      = "CNAME"
+  content   = "474609.selcdn.ru"
+  ttl       = 60
+}
+
+resource "selectel_domains_record_v1" "cloudcontent_cname_records" {
+  for_each  = module.bitrix
+  domain_id = selectel_domains_domain_v1.domain.id
+  name      = "cloudcontent.${local.dns_domain}"
+  type      = "CNAME"
+  content   = "474609.selcdn.ru"
+  ttl       = 60
+}
